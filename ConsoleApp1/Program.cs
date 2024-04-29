0411d42315e7521535e3c2f5c8074edc9cef5f8f
MMallory17 <mdmallory87@gmail.com>
2024-04-29 15:11:57 -0400

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Welcome to Grand Circus' Room Detail Generator!");
+
+
+Console.WriteLine("Enter Length");
+int Length = (int)Decimal.Parse(Console.ReadLine());
+Console.WriteLine("Enter Width");
+int Width = (int)Decimal.Parse(Console.ReadLine());
+int Area = Length * Width;
+Console.WriteLine("calculated " + Area);
+int Perimeter = 2 * (Length + Width);
+Console.WriteLine("calculated " + Perimeter);
+
+{
+    if (Area <= 250)
+        Console.WriteLine("calculated this is a small-sized room.");
+
+    else if (25 < Area) _ =(Area < 650);
+   Console.WriteLine("calculated this is a medium-sized room.");
+
+if (650 <= Area)
+    Console.WriteLine("calulated this is a large-sized room.");
+}
+
+Console.WriteLine("Thank you for using the Room Detail Generator!");
+
+        
